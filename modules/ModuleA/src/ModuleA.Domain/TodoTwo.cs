a52245693a6c46db32eb3e7a3c533664bfc2939a
Anto Subash <antosubash@live.com>
2021-11-04 17:23:54 +0100

    Create TodoTwo.cs

---
+++ modules/ModuleA/src/ModuleA.Domain/TodoTwo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Domain.Entities;
+
+namespace ModuleA
+{
+    public class TodoTwo : Entity<Guid>
+    {
+        public string Content { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
